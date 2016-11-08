9cc97f953839cbf9e6bb6df1e8efcf1caf7673bc
Josef Kuchař <email@josefkuchar.cz>
2016-11-08 22:29:51 +0100

    Rewrite comments

--- KomendImp.user.js
+++ KomendImp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KomendImp
 // @namespace    http://www.josefkuchar.cz/
-// @version      1.2.0
+// @version      1.2.1
 // @description  Some improvement for http://komenskehozsnachod.cz/
 // @author       Josef Kuchař
 // @match        http://www.komenskehozsnachod.cz/*
@@ -10,10 +10,10 @@
 // ==/UserScript==
 
 (function() {
-  var html = $(".clt").html();
-  html = html.replace(/[-]{5,}/g,"<hr>"); //Replace ---------- to <hr>
-  html = html.replace(/[_]{5,}/g,"<hr>"); //Replace __________ to <hr>
-  html = html.replace(/[+]{5,}/g,"<hr>"); //Replace ++++++++++ to <hr>
-  html = html.replace(/&nbsp;/gi," "); //Replace &nbsp; with space
-  $(".clt").html(html);
+  var html = $(".clt").html(); // Get content of the page
+  html = html.replace(/[-]{5,}/g,"<hr>"); // Replace ---------- with <hr>
+  html = html.replace(/[_]{5,}/g,"<hr>"); // Replace __________ with <hr>
+  html = html.replace(/[+]{5,}/g,"<hr>"); // Replace ++++++++++ with <hr>
+  html = html.replace(/&nbsp;/gi," "); // Replace &nbsp; with space
+  $(".clt").html(html); // Apply changes
 })();
