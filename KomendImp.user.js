2730ee8a1799baea5b53879d0d51bdb7ce673953
Josef Kuchař <email@josefkuchar.cz>
2016-11-08 20:34:36 +0100

    Add +++ to hr

--- KomendImp.user.js
+++ KomendImp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         KomendImp
 // @namespace    http://www.josefkuchar.cz/
-// @version      1.1.0
+// @version      1.2.0
 // @description  Some improvement for http://komenskehozsnachod.cz/
 // @author       Josef Kuchař
 // @match        http://www.komenskehozsnachod.cz/*
@@ -13,6 +13,7 @@
   var html = $(".clt").html();
   html = html.replace(/[-]{5,}/g,"<hr>"); //Replace ---------- to <hr>
   html = html.replace(/[_]{5,}/g,"<hr>"); //Replace __________ to <hr>
+  html = html.replace(/[+]{5,}/g,"<hr>"); //Replace ++++++++++ to <hr>
   html = html.replace(/&nbsp;/gi," "); //Replace &nbsp; with space
   $(".clt").html(html);
 })();
